eee65c2b438e8f28b1ed949737c12145c9572699
Yogeswaran <yogeeswaran@gmail.com>
2015-11-29 06:18:46 +0530

    Added support to export report as csv - for now dummy report

--- server/aiserverproj/aiserverproj/urls.py
+++ server/aiserverproj/aiserverproj/urls.py
@@ -27,10 +27,12 @@ router.register(r'child', views.ChildViewSet)
 router.register(r'skill', views.SkillViewSet)
 router.register(r'assessment', views.AssessmentViewSet)
 
+
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'export', views.center_based_report),
 ]
 
