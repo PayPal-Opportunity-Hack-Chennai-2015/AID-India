b678cd437a9f83c0c553d3c8ee269aa954bf9483
PALANIAPPAN N <paln@cisco.com>
2015-11-29 12:48:53 +0530

    oi

--- client/android/euraka1/app/src/main/java/com/eurekakids/euraka1/tam_fragment.java
+++ client/android/euraka1/app/src/main/java/com/eurekakids/euraka1/tam_fragment.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Switch;
 
+import com.eurekakids.com.eurekakids.db.DatabaseHandler;
 import com.eurekakids.db.datamodel.Assessment;
 import com.eurekakids.db.datamodel.Student;
 
@@ -35,7 +36,25 @@ public class tam_fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.tam_fragment,container,false);
+		ViewGroup viewGroup = (ViewGroup) v;
+		ViewGroup linear = (ViewGroup) viewGroup.getChildAt(0);
+		int view_count = linear.getChildCount();
 
+		Bundle args = getArguments();
+		int child_id = args.getInt("id", 0);
+		int skill = 1;
+		for (int i = 0; i < view_count; i++) {
+			View view = linear.getChildAt(i);
+			DatabaseHandler db = new DatabaseHandler(getActivity());
+			Assessment ass = db.getAssessmentById(child_id, skill);
+			int t = ass.getIsCompleted();
+			if(t==0){
+				((Switch) view).setChecked(false);
+			}else{
+				((Switch) view).setChecked(true);
+			}
+			skill++;
+		}
 
         return v;
     }
